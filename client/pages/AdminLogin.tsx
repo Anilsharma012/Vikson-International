45477a28b67fb9e555f1e1be4cab2dce12b8556f
Ishwar78 <ishwarsharma27092002@gmail.com>
2025-07-16 14:25:19 +0530

    push agaiin

--- client/pages/AdminLogin.tsx
+++ client/pages/AdminLogin.tsx
@@ -12,7 +12,7 @@ const AdminLogin = () => {
     setError("");
 
     try {
-      const res = await fetch("http://localhost:8081/api/auth/login", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
